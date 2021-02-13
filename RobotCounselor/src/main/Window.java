16c9aaa3153262348d7bcaade1f0031095e8e469
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2021-02-13 14:15:13 -0500

    gui framework
    added checklist, add button, delete button, and update button

--- RobotCounselor/src/main/Window.java
+++ RobotCounselor/src/main/Window.java
@@ -5,6 +5,7 @@
  */
 package main;
 
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,11 +14,14 @@ import javax.swing.JOptionPane;
  */
 public class Window extends javax.swing.JFrame {
 
+    ArrayList<String> classes = new ArrayList<>();
+    
     /**
      * Creates new form Window
      */
     public Window() {
         initComponents();
+        setResizable(false);
     }
 
     /**
@@ -31,20 +35,82 @@ public class Window extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         inputPanel = new javax.swing.JPanel();
-        nameButton = new javax.swing.JButton();
+        algebraBox = new javax.swing.JCheckBox();
+        bandBox = new javax.swing.JCheckBox();
+        englishBox = new javax.swing.JCheckBox();
+        healthBox = new javax.swing.JCheckBox();
+        ssBox = new javax.swing.JCheckBox();
+        frenchBox = new javax.swing.JCheckBox();
+        chemistryBox = new javax.swing.JCheckBox();
+        artBox = new javax.swing.JCheckBox();
+        gymBox = new javax.swing.JCheckBox();
+        addClassesButton = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        studentsInfo = new javax.swing.JTextArea();
+        deleteButton = new javax.swing.JButton();
+        updateButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Robot Counselor");
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        jLabel1.setText("Robot Counselor");
+        jLabel1.setText("Scheduling System");
 
         inputPanel.setName("Options"); // NOI18N
 
-        nameButton.setText("Name");
-        nameButton.addActionListener(new java.awt.event.ActionListener() {
+        algebraBox.setText("Algebra");
+
+        bandBox.setText("Band");
+
+        englishBox.setText("English");
+        englishBox.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nameButtonActionPerformed(evt);
+                englishBoxActionPerformed(evt);
+            }
+        });
+
+        healthBox.setText("Health");
+
+        ssBox.setText("Social Studies");
+
+        frenchBox.setText("French");
+
+        chemistryBox.setText("Chemistry");
+
+        artBox.setText("Art");
+
+        gymBox.setText("Gym");
+
+        addClassesButton.setText("Add");
+        addClassesButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addClassesButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Add Classes:");
+
+        studentsInfo.setEditable(false);
+        studentsInfo.setColumns(20);
+        studentsInfo.setRows(5);
+        studentsInfo.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Student Info"));
+        studentsInfo.setDisabledTextColor(new java.awt.Color(0, 0, 0));
+        studentsInfo.setEnabled(false);
+        studentsInfo.setFocusable(false);
+        jScrollPane1.setViewportView(studentsInfo);
+
+        deleteButton.setText("Delete");
+        deleteButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteButtonActionPerformed(evt);
+            }
+        });
+
+        updateButton.setText("Update");
+        updateButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateButtonActionPerformed(evt);
             }
         });
 
@@ -53,16 +119,62 @@ public class Window extends javax.swing.JFrame {
         inputPanelLayout.setHorizontalGroup(
             inputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(inputPanelLayout.createSequentialGroup()
-                .addGap(159, 159, 159)
-                .addComponent(nameButton)
-                .addContainerGap(181, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(inputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(englishBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(bandBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(inputPanelLayout.createSequentialGroup()
+                        .addGroup(inputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(deleteButton)
+                            .addComponent(addClassesButton)
+                            .addGroup(inputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(gymBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(artBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(chemistryBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(frenchBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(ssBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabel2)
+                            .addComponent(algebraBox, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(healthBox, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(updateButton))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         inputPanelLayout.setVerticalGroup(
             inputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(inputPanelLayout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(nameButton)
-                .addContainerGap(448, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(inputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(inputPanelLayout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(algebraBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bandBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(englishBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(healthBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(ssBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(frenchBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(chemistryBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(artBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(gymBox)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(addClassesButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(deleteButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(updateButton))
+                    .addComponent(jScrollPane1))
+                .addGap(32, 32, 32))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -70,30 +182,30 @@ public class Window extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(348, 348, 348)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(266, 266, 266)
-                        .addComponent(inputPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(311, Short.MAX_VALUE))
+                    .addComponent(jLabel1)
+                    .addComponent(inputPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(12, 12, 12)
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(inputPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(inputPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void nameButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameButtonActionPerformed
-         String s = (String)JOptionPane.showInputDialog(
+    private void englishBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_englishBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_englishBoxActionPerformed
+
+    private void addClassesButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addClassesButtonActionPerformed
+        
+        String name = (String)JOptionPane.showInputDialog(
                     inputPanel,
                     "Enter student name:",
                     "Student Name",
@@ -101,12 +213,66 @@ public class Window extends javax.swing.JFrame {
                     null,
                     null,
                     "John");
+         
+        if ((name != null) && (name.length() > 0)) {
+            //Write name to database and text field
+        }       
+        
+        if (algebraBox.isSelected()) {classes.add("Algebra");}
+        if (bandBox.isSelected()) {classes.add("Band");}
+        if (englishBox.isSelected()) {classes.add("English");}
+        if (healthBox.isSelected()) {classes.add("Health");}
+        if (ssBox.isSelected()) {classes.add("Social Studies");}
+        if (frenchBox.isSelected()) {classes.add("French");}
+        if (chemistryBox.isSelected()) {classes.add("Chemistry");}
+        if (artBox.isSelected()) {classes.add("Art");}
+        if (gymBox.isSelected()) {classes.add("Gym");}
+        System.out.println("Name is: " + name);
+        System.out.println(classes);
+    }//GEN-LAST:event_addClassesButtonActionPerformed
 
-//If a string was returned, say so.
-if ((s != null) && (s.length() > 0)) {
-    
-}
-    }//GEN-LAST:event_nameButtonActionPerformed
+    private void updateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateButtonActionPerformed
+        String name = (String)JOptionPane.showInputDialog(
+                    inputPanel,
+                    "Enter student name to update:",
+                    "Update Student",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    "John");
+         
+        if ((name != null) && (name.length() > 0)) {
+            Object[] options = {"Name","Classes"};
+            int selection = JOptionPane.showOptionDialog(inputPanel,
+                    "Do you want to update name or classes?",
+                    "Update Student",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options, 
+                    null);
+            if (selection == 0){
+                //Let them change name and update in database
+            } else if (selection == 1) {
+                //Let them change the classes, auto show which are already checked
+            }   
+        }
+    }//GEN-LAST:event_updateButtonActionPerformed
+
+    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
+        String name = (String)JOptionPane.showInputDialog(
+                    inputPanel,
+                    "Enter student name to remove:",
+                    "Update Student",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    "John");
+        
+        if ((name != null) && (name.length() > 0)) {
+            //Write name to database and text field
+        } 
+    }//GEN-LAST:event_deleteButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -144,8 +310,22 @@ if ((s != null) && (s.length() > 0)) {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addClassesButton;
+    private javax.swing.JCheckBox algebraBox;
+    private javax.swing.JCheckBox artBox;
+    private javax.swing.JCheckBox bandBox;
+    private javax.swing.JCheckBox chemistryBox;
+    private javax.swing.JButton deleteButton;
+    private javax.swing.JCheckBox englishBox;
+    private javax.swing.JCheckBox frenchBox;
+    private javax.swing.JCheckBox gymBox;
+    private javax.swing.JCheckBox healthBox;
     private javax.swing.JPanel inputPanel;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JButton nameButton;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JCheckBox ssBox;
+    private javax.swing.JTextArea studentsInfo;
+    private javax.swing.JButton updateButton;
     // End of variables declaration//GEN-END:variables
 }
