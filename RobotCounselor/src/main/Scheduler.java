eef359e94e474ee97b1bd202e4325345a7e31e15
ChopinGang <kellan.elhai@gmail.com>
2021-02-13 16:20:57 -0500

    Update Scheduler.java

--- RobotCounselor/src/main/Scheduler.java
+++ RobotCounselor/src/main/Scheduler.java
@@ -6,13 +6,48 @@
 
 package main;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kellan Elhai
  */
 public class Scheduler {
-    public static schedulePerson(ArrayList<String> person) {
-        //do something
+    
+    public static void schedulePerson(ArrayList<String> person) {
+        ArrayList<ArrayList<String>> period = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            period.add(new ArrayList<String>());
+        }
+        
+        init(period.get(0), "English", "Chemistry", "Med Tech", "Economics");
+        init(period.get(0), "Band", "French", "Orchestra", "Debate");
+        init(period.get(0), "Algebra", "Social Studies", "Engineering", "Statistics");
+        init(period.get(0), "Band", "Gym", "Orchestra", "Med Tech");
+        init(period.get(0), "Algebra", "French", "Theater", "Statistics");
+        init(period.get(0), "Health", "Social Studies", "Debate", "Horticulture");
+        init(period.get(0), "English", "Chemistry", "Engineering", "Economics");
+        init(period.get(0), "Health", "Art", "Coding", "Horticulture");
+        init(period.get(0), "Art", "Gym", "Coding", "Theater");
+        
+        String name = person.get(0);
+        ArrayList<String> classes = new ArrayList<>();
+        for (int i = 1; i < person.size(); i++) {
+            classes.add(person.get(i));
+        }
+        
+        for (int i = 0; i < period.size(); i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println(period.get(i).get(j));
+            }
+        }
+    }
+    
+    private static void init(ArrayList<String> list, String c1, String c2, String c3, String c4) {
+        list.add(c1);
+        list.add(c2);
+        list.add(c3);
+        list.add(c4);
     }
     
 }
